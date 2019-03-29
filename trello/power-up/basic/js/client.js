29e166b511ce7b1a3df8ac7faae2087b1bc0f52a
Adrian Eng Ying Ming <adrian.engyingming@gmail.com>
2019-03-29 10:34:27 +0800

    Use get token instead

--- trello/power-up/basic/js/client.js
+++ trello/power-up/basic/js/client.js
@@ -229,6 +229,19 @@ var cardButtonUpdateTitle = function (t) {
             }
         });
 
+    t.get('member', 'private', 'token')
+        .then(function (token) {
+            if (!token) {
+                console.log("YOURE NOT AUTH")
+            } else {
+                console.log("Getting list of members")
+                const url = `https://api.trello.com/1/members/me/boards?` +
+                    `key=9ed85a487eb0b08bff2f11e84cc80c16&token=${token}`
+                $.get(url);
+            }
+        });
+
+
     // t.card('all')
     //     .then(function (card) {
     //         console.log(JSON.stringify(card, null, 2));

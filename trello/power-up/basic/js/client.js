8f08a3879aad14ae5a470b5abf8db5a2e49255dd
Adrian Eng Ying Ming <adrian.engyingming@gmail.com>
2019-03-31 16:38:13 +0800

    Filter to stuff i want

--- trello/power-up/basic/js/client.js
+++ trello/power-up/basic/js/client.js
@@ -3,79 +3,6 @@
 // we can access Bluebird Promises as follows
 var Promise = TrelloPowerUp.Promise;
 
-/*
-
-Trello Data Access
-
-The following methods show all allowed fields, you only need to include those you want.
-They all return promises that resolve to an object with the requested fields.
-
-Get information about the current board
-t.board('id', 'name', 'url', 'shortLink', 'members')
-
-Get information about the current list (only available when a specific list is in context)
-So for example available inside 'attachment-sections' or 'card-badges' but not 'show-settings' or 'board-buttons'
-t.list('id', 'name', 'cards')
-
-Get information about all open lists on the current board
-t.lists('id', 'name', 'cards')
-
-Get information about the current card (only available when a specific card is in context)
-So for example available inside 'attachment-sections' or 'card-badges' but not 'show-settings' or 'board-buttons'
-t.card('id', 'name', 'desc', 'due', 'closed', 'cover', 'attachments', 'members', 'labels', 'url', 'shortLink', 'idList')
-
-Get information about all open cards on the current board
-t.cards('id', 'name', 'desc', 'due', 'closed', 'cover', 'attachments', 'members', 'labels', 'url', 'shortLink', 'idList')
-
-Get information about the current active Trello member
-t.member('id', 'fullName', 'username')
-
-For access to the rest of Trello's data, you'll need to use the RESTful API. This will require you to ask the
-user to authorize your Power-Up to access Trello on their behalf. We've included an example of how to
-do this in the `🔑 Authorization Capabilities 🗝` section at the bottom.
-
-*/
-
-/*
-
-Storing/Retrieving Your Own Data
-
-Your Power-Up is afforded 4096 chars of space per scope/visibility
-The following methods return Promises.
-
-Storing data follows the format: t.set('scope', 'visibility', 'key', 'value')
-With the scopes, you can only store data at the 'card' scope when a card is in scope
-So for example in the context of 'card-badges' or 'attachment-sections', but not 'board-badges' or 'show-settings'
-Also keep in mind storing at the 'organization' scope will only work if the active user is a member of the team
-
-Information that is private to the current user, such as tokens should be stored using 'private' at the 'member' scope
-
-t.set('organization', 'private', 'key', 'value');
-t.set('board', 'private', 'key', 'value');
-t.set('card', 'private', 'key', 'value');
-t.set('member', 'private', 'key', 'value');
-
-Information that should be available to all users of the Power-Up should be stored as 'shared'
-
-t.set('organization', 'shared', 'key', 'value');
-t.set('board', 'shared', 'key', 'value');
-t.set('card', 'shared', 'key', 'value');
-t.set('member', 'shared', 'key', 'value');
-
-If you want to set multiple keys at once you can do that like so
-
-t.set('board', 'shared', { key: value, extra: extraValue });
-
-Reading back your data is as simple as
-
-t.get('organization', 'shared', 'key');
-
-Or want all in scope data at once?
-
-t.getAll();
-
-*/
-
 const HELPER = window.helper({key: "9ed85a487eb0b08bff2f11e84cc80c16"});
 
 var GLITCH_ICON = './images/glitch.svg';
@@ -198,25 +125,30 @@ var boardButtonCallback = function (t) {
 var cardButtonUpdateTitle = async function (t) {
 
     const boards = await HELPER.my.getBoards(t);
+    const {board, card} = await HELPER.getContext(t)
 
     const results = await t.getAll();
 
     console.log("Boards:", boards)
-    // for (const index in boards) {
-    //     const board = boards[index];
-    //     const {id} = board;
-    //     const lists = await HELPER.board.getLists(t, {board: id});
-    //     const labels = await HELPER.board.getLabels(t, {board: id});
-    //     const members = await HELPER.board.getMembers(t, {board: id});
-    //     console.log("Booard", board,
-    //         "members: ", members,
-    //         "List: ", lists,
-    //         "Labels: ", labels);
-    // }
+    const boardsIWant = boards.filter(function(d){
+        return d.id == board;
+    })
+
+    for (const index in boardsIWant) {
+        const board = boards[index];
+        const {id} = board;
+        const lists = await HELPER.board.getLists(t, {board: id});
+        const labels = await HELPER.board.getLabels(t, {board: id});
+        const members = await HELPER.board.getMembers(t, {board: id});
+        console.log("Booard", board,
+            "members: ", members,
+            "List: ", lists,
+            "Labels: ", labels);
+    }
 
 
     console.log("let the magic beginds");
-    const {board, card} = await HELPER.getContext(t)
+
     const cardName = await HELPER.card.getName(t, {card});
     const members = await HELPER.board.getMembers(t, {board});
     const newCardName = cardName + " 1";

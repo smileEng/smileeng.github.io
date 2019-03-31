60c953cda0a03f0c0db9b95f3c70084bbab2548a
Adrian Eng Ying Ming <adrian.engyingming@gmail.com>
2019-03-31 18:58:36 +0800

    set values

--- trello/power-up/basic/js/settings.js
+++ trello/power-up/basic/js/settings.js
@@ -7,24 +7,20 @@ const jsonUrlInputText = document.getElementById('jsonUrl');
 // var vegetableSelector = document.getElementById('vegetable');
 
 t.render(function () {
-    t.sizeTo('#content').done();
+    //https://smileeng.github.io/trello/power-up/basic/configs/board/trello-powerups/card-buttons.json
 
-    // return Promise.all([
-    //     t.get('board', 'shared', 'fruit'),
-    //     t.get('board', 'private', 'vegetable')
-    // ])
-    //     .spread(function (savedFruit, savedVegetable) {
-    //         if (savedFruit && /[a-z]+/.test(savedFruit)) {
-    //             fruitSelector.value = savedFruit;
-    //         }
-    //         if (savedVegetable && /[a-z]+/.test(savedVegetable)) {
-    //             vegetableSelector.value = savedVegetable;
-    //         }
-    //     })
-    //     .then(function () {
-    //         t.sizeTo('#content')
-    //             .done();
-    //     })
+
+    return Promise.all(
+        [
+            t.get('board', 'private', 'jsonUrl')
+        ])
+        .spread(function (jsonUrl) {
+            jsonUrlInputText.value = jsonUrl;
+        })
+        .then(function () {
+            t.sizeTo('#content')
+                .done();
+        })
 });
 
 document
